893b0455b1789d77209122919a30174637202011
PureChaos712 <wbulanda712@gmail.com>
2023-05-25 16:58:47 +0200

    Ukonczone ex5.py

--- ex5.py
+++ ex5.py
@@ -4,7 +4,9 @@ user = "purechaos712"
 response = httpx.get(f"https://api.github.com/users/{user}/repos")
 repos = response.json()
 
-print(repos)
+for repo in repos:
+    print("Project:", repo.get('name'))
+    print("URL:", repo.get('url'))
 
 #git status - status, duh
 #git add - dodanie "do poczekalni"
