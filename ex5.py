d7aca6626e3c5bab1dba5981688643afec170be6
PureChaos712 <wbulanda712@gmail.com>
2023-05-25 16:50:26 +0200

    Edit ex5.py

--- ex5.py
+++ ex5.py
@@ -8,6 +8,5 @@ print(repos)
 
 #git status - status, duh
 #git add - dodanie "do poczekalni"
-#git commit -m "Add <<plik>>"
-#git commit -m "Edit <<ex5.py>>"
+#git commit -m "A message describing what you have done to make this snapshot different"
 #git push - wypchnięcie do repo
